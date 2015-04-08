f044326f07deec0da3f3007fe409020d81642822
Klas Eskilson <klas.eskilson@gmail.com>
2015-04-08 21:01:03 +0200

    Add home, index json and add methods

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # import our packages
 import sqlite3
-from flask import Flask
+from flask import Flask, request, g, render_template, jsonify, flash
 from contextlib import closing
 
 # setup app with config
@@ -30,6 +30,42 @@ def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
+
+@app.route('/')
+def show_home():
+    return render_template('home.html')
+
+@app.route('/todos.json')
+def show_todos():
+    # get cursor of results
+    cur = g.db.execute('select id, title, completed, below from todos order \
+                        by below asc')
+    # fetch the entries
+    todos = cur.fetchall()
+    json_results = []
+    # iterate over todos
+    for todo in todos:
+        t = {'id': todo[0],
+             'title': todo[1],
+             'completed': todo[2],
+             'below': todo[3]}
+        json_results.append(t)
+    # respond with json
+    return jsonify(todos = json_results)
+
+@app.route('/add.json', methods=['POST'])
+def add_todo():
+    # try to save
+    try:
+        g.db.execute('insert into todos (title, below) values (?, ?)',
+            [request.form['title'], request.form['below']])
+        g.db.commit()
+        # prep response
+        resp = jsonify(message='created todo')
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        return jsonify({"response": "ERROR %s" % str(e)})
 
 if __name__ == '__main__':
     app.run()
